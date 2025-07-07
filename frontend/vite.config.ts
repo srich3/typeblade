7f007a0075a28587f42a1fc7e8661ae7ca56e6e2
srich3 <monsterchug@gmail.com>
2025-07-06 17:18:30 -0700

    Remove typecheck from build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 import { configDefaults } from 'vitest/config'
-
+import checker from 'vite-plugin-checker';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    checker({ typescript: true })
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
